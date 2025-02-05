8a4b4b683414c7638da72881fbb73410eaed5050
andrii <andriiivanchov@gmail.com>
2025-02-05 15:32:38 +0300

    Add: a map in the Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -24,6 +24,7 @@ export default function Home(): JSX.Element {
             <Reviews />
             <Gallery />
             <Subscribe />
+            <iframe className={styles.map} src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d52911152.012123756!2d-161.65545109903397!3d35.960656281788644!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sus!4v1738758474204!5m2!1sru!2sus" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
         </main>
     );
 }
