4d58d96be4c64ba4f44ffbaed895f4dc8dadaffc
andrii <andriiivanchov@gmail.com>
2025-02-25 20:31:16 +0300

    Refact: the 'Card' component
    - Added "CounterCard", "HeaderCard" components to slit logic and to make
    the main component more clearer and readable

---
+++ src/containers/Card/components/CounterCard/CounterCard.tsx
@@ -0,0 +1,37 @@
+import styles from './styles.module.scss';
+import gStyles from '../../../../styles/styles.module.scss';
+import Counter from '../../../../components/ui/Counter/Counter';
+import { useId } from 'react';
+import { IBasketProduct } from '../../../../interface/interface';
+import { addProduct } from '../../../../features/basket/basket';
+import { useAppDispatch } from '../../../../hooks/useAppDispatch';
+
+interface IProps {
+    name: string;
+    manufacturer: string;
+    price: number;
+    isRecipe: boolean;
+    countryOrigin: string;
+    counter: number;
+    setCounter: React.Dispatch<React.SetStateAction<number>>;
+}
+
+export default function CounterCard(props: IProps): JSX.Element {
+
+    const { name, manufacturer, countryOrigin, price, isRecipe, setCounter, counter } = props;
+
+    const uniqueId = useId();
+
+    const dispatch = useAppDispatch();
+
+    function handlePutProduct(productBasket: IBasketProduct) {
+        dispatch(addProduct(productBasket));
+    }
+
+    return (
+        <div className={styles.buttonBody}>
+            <Counter value={counter} setValue={setCounter} />
+            <button onClick={() => handlePutProduct({ id: uniqueId, name, manufacturer, countryOrigin, isRecipe, image: "", price, count: counter })} className={`${styles.button} ${gStyles.textMedium}`}>В корзину</button>
+        </div>
+    );
+}
